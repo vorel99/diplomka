ea292cca5d17a72180c5263e2cf8dea8e497521d
Jan Cap <vorel007@gmail.com>
2026-01-14 16:57:32 +0100

    update load raw election add AGS code

--- src/geoscore_de/data_flow/election/federal_21.py
+++ src/geoscore_de/data_flow/election/federal_21.py
@@ -7,8 +7,11 @@ from geoscore_de.data_flow.election.utils import load_election_zip, move_extract
 
 ZIP_URL = "https://www.bundeswahlleiterin.de/en/dam/jcr/c2cd99e6-064e-4ebc-b634-f86b5c0e14b3/btw21_wbz.zip"
 
+DEFAULT_RAW_DATA_PATH = "data/raw/features/election_2021"
+DEFAULT_TFORM_DATA_PATH = "data/tform/features/federal_election_21.csv"
 
-def load_raw_election_21_data(url: str = ZIP_URL, dest_path: str = "data/raw/election_2021") -> pd.DataFrame:
+
+def load_raw_election_21_data(url: str = ZIP_URL, dest_path: str = DEFAULT_RAW_DATA_PATH) -> pd.DataFrame:
     """Load and extract election 21 data from a ZIP file.
 
     Args:
@@ -29,5 +32,16 @@ def load_raw_election_21_data(url: str = ZIP_URL, dest_path: str = "data/raw/ele
 
     # Load the election data CSV into a DataFrame
     election_csv_path = Path(dest_path) / "btw21_wbz_ergebnisse.csv"
-    df = pd.read_csv(election_csv_path, sep=";", encoding="latin1")
+    dtypes = {
+        "Land": str,
+        "Regierungsbezirk": str,
+        "Kreis": str,
+        "Gemeinde": str,
+    }
+    df = pd.read_csv(election_csv_path, sep=";", dtype=dtypes)
+
+    # create AGS column by concatenating Land, Regierungsbezirk, Kreis, and Gemeinde columns
+    # Convert to string first to handle potential numeric types
+    df["AGS"] = df["Land"] + df["Regierungsbezirk"] + df["Kreis"] + df["Gemeinde"].str.zfill(3)
+
     return df
