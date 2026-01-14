6c0d5d45e084559046853cac29019c0c86bade57
Jan Cap <71695857+vorel99@users.noreply.github.com>
2026-01-14 10:00:58 +0100

    5 target load (#6)
    - add migration data
    - add unemployment data

---
+++ src/geoscore_de/data_flow/migration.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+def load_migration_data(path: str) -> pd.DataFrame:
+    """Load migration data from a CSV file.
+    Data were obtained from the GENESIS-Online regional statistics database of the German Federal Statistical Office.
+    https://www.regionalstatistik.de/genesis//online?operation=table&code=12711-91-01-5&bypass=true&levelindex=0&levelid=1768376496916#abreadcrumb
+
+    Args:
+        path (str): Path to the CSV file.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the loaded migration data.
+    """
+    df = pd.read_csv(
+        path,
+        sep=";",
+        encoding="latin1",
+        skiprows=4,
+        skipfooter=4,
+        engine="python",
+        na_values=["-", "."],
+    )
+    df.rename(
+        columns={
+            "Unnamed: 0": "Year",
+            "Unnamed: 1": "MU_ID",
+            "Unnamed: 2": "Municipality",
+            "Zuzüge über die Gemeindegrenzen": "in_migration",
+            "Fortzüge über die Gemeindegrenzen": "out_migration",
+        },
+        inplace=True,
+    )
+
+    # drop first row which contains column descriptions
+    df = df.iloc[1:].reset_index(drop=True)
+
+    df["MU_ID"] = df["MU_ID"].astype(str)
+    df["in_migration"] = pd.to_numeric(df["in_migration"])
+    df["out_migration"] = pd.to_numeric(df["out_migration"])
+
+    # fill MU_ID on the right with zeros to a total length of 8 characters
+    df["AGS"] = df["MU_ID"].str.ljust(8, "0")
+
+    return df
---
+++ src/geoscore_de/data_flow/population.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+def load_population_data(path: str) -> pd.DataFrame:
+    """Load population data from a CSV file.
+    Data were obtained from https://www.regionalstatistik.de/genesis//online?operation=table&code=12411-02-03-5&bypass=true&levelindex=1&levelid=1765292926381#abreadcrumb
+
+    Args:
+        path (str): Path to the CSV file.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the loaded population data.
+    """
+    df = pd.read_csv(
+        path,
+        sep=";",
+        encoding="latin1",
+        skiprows=5,
+        skipfooter=4,
+        engine="python",
+        na_values=["-", "."],
+    )
+    df.rename(
+        columns={
+            "Unnamed: 0": "date",
+            "Unnamed: 1": "MU_ID",
+            "Unnamed: 2": "Municipality",
+            "Unnamed: 3": "age_group",
+            "Insgesamt": "people_count",
+            "männlich": "male_count",
+            "weiblich": "female_count",
+        },
+        inplace=True,
+    )
+
+    # add AGS column by right-padding MU_ID with zeros to 8 characters (adds trailing zeros if necessary)
+    df["AGS"] = df["MU_ID"].str.ljust(8, "0")
+
+    return df
