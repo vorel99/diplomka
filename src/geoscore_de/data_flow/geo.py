a15513f85854baedf15ea8f79c51f3a6fda710e6
Jan Cap <vorel007@gmail.com>
2026-04-02 13:09:20 +0200

    fix geo tests

---
+++ src/geoscore_de/data_flow/geo.py
@@ -0,0 +1,22 @@
+"""Helpers for loading geospatial data into GeoDataFrames."""
+
+from __future__ import annotations
+
+import json
+
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import shape
+
+
+def load_geo_data(path: str) -> gpd.GeoDataFrame:
+    """Load geospatial data from a file into a GeoDataFrame.
+    Supported format is csv from OpenDataSoft
+    """
+    ods_gdf_mun = gpd.read_file(path)
+    ods_gdf_mun["geometry"] = ods_gdf_mun["Geo Shape"].apply(lambda x: shape(json.loads(x)) if pd.notnull(x) else None)
+    ods_gdf_mun = ods_gdf_mun.drop(columns=["Geo Shape"])
+    ods_gdf_mun = gpd.GeoDataFrame(ods_gdf_mun, geometry="geometry", crs="EPSG:4326")
+    # add AGS column
+    ods_gdf_mun["AGS"] = ods_gdf_mun["Gemeinde code"].str.slice(0, 5) + ods_gdf_mun["Gemeinde code"].str.slice(9, 12)
+    return ods_gdf_mun
