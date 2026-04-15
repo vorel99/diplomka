72ca2008f07f7a2a3eda6c4831c67dcef71604c2
Jan Cap <vorel007@gmail.com>
2026-04-15 09:26:48 +0200

    remove deprecated func

--- src/geoscore_de/data_flow/features/migration.py
+++ src/geoscore_de/data_flow/features/migration.py
@@ -16,6 +16,8 @@ class MigrationFeature(BaseFeature):
 
     def load(self) -> pd.DataFrame:
         """Load migration data from a CSV file.
+        Data obtained from the GENESIS-Online regional statistics database of the German Federal Statistical Office.
+        https://www.regionalstatistik.de/genesis//online?operation=table&code=12711-91-01-5&bypass=true&levelindex=0&levelid=1768376496916#abreadcrumb
 
         Returns:
             pd.DataFrame: DataFrame containing the loaded migration data.
@@ -72,36 +74,3 @@ class MigrationFeature(BaseFeature):
 
         pivoted.columns = [f"{metric}_{year}" for metric, year in pivoted.columns]
         return pivoted.reset_index()
-
-
-def load_migration_data(path: str) -> pd.DataFrame:
-    """Load migration data from a CSV file.
-    Data were obtained from the GENESIS-Online regional statistics database of the German Federal Statistical Office.
-    https://www.regionalstatistik.de/genesis//online?operation=table&code=12711-91-01-5&bypass=true&levelindex=0&levelid=1768376496916#abreadcrumb
-
-    Args:
-        path (str): Path to the CSV file.
-
-    Returns:
-        pd.DataFrame: DataFrame containing the loaded migration data.
-    """
-    df = pd.read_csv(
-        path,
-        sep=";",
-        encoding="latin1",
-        skiprows=6,
-        skipfooter=4,
-        engine="python",
-        na_values=["-", "."],
-        names=["Year", "MU_ID", "Municipality", "in_migration", "out_migration"],
-        header=None,
-    )
-
-    df["MU_ID"] = df["MU_ID"].astype(str)
-    df["in_migration"] = pd.to_numeric(df["in_migration"])
-    df["out_migration"] = pd.to_numeric(df["out_migration"])
-
-    # fill MU_ID on the right with zeros to a total length of 8 characters
-    df["AGS"] = df["MU_ID"].str.ljust(8, "0")
-
-    return df
