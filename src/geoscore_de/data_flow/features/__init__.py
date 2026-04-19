b75899a5a6bdc6c63a7b7a98c387ba9fda3f4de5
Jan Cap <vorel007@gmail.com>
2026-04-19 15:31:19 +0200

    add osm hospital feature

--- src/geoscore_de/data_flow/features/__init__.py
+++ src/geoscore_de/data_flow/features/__init__.py
@@ -4,6 +4,7 @@ from geoscore_de.data_flow.features.birth import BirthFeature
 from geoscore_de.data_flow.features.election_21 import Election21Feature
 from geoscore_de.data_flow.features.election_25 import Election25Feature
 from geoscore_de.data_flow.features.migration import MigrationFeature
+from geoscore_de.data_flow.features.osm_hospitals import OSMHospitalsFeature
 from geoscore_de.data_flow.features.population import PopulationFeature
 from geoscore_de.data_flow.features.road_accidents import RoadAccidentsFeature
 from geoscore_de.data_flow.features.unemployment import UnemploymentFeature
@@ -16,6 +17,7 @@ __all__ = [
     "Election21Feature",
     "Election25Feature",
     "MigrationFeature",
+    "OSMHospitalsFeature",
     "PopulationFeature",
     "RoadAccidentsFeature",
     "UnemploymentFeature",
